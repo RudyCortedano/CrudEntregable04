a3f3b7d382230b5043b4cbb03d8b1cf6608afd93
RudyCortedano <rudycortedano028@gmail.com>
2023-11-29 14:23:38 -0600

    13.baseUrl

--- src/context/CrudContext.jsx
+++ src/context/CrudContext.jsx
@@ -9,9 +9,9 @@ const CrudProvider = (props) => {
   // logica del  CRUD
   const [updateInfo, setUpdateInfo] = useState();
 
-  // const baseUrl = `https://users-crud.academlo.tech`;  //? API Usada en el modulo de front-end
+  const baseUrl = `https://users-crud.academlo.tech`;  //? API Usada en el modulo de front-end
   // const baseUrl = `https://users-dev-hfnd.3.us-1.fl0.io`; //? API Creada en el modulo de back-end
-  const baseUrl = `https://users-crud-backend-rchj.onrender.com`; //? API Creada en el modulo de back-end
+  // const baseUrl = `https://users-crud-backend-rchj.onrender.com`; //? API Creada en el modulo de back-end
 
     // hook personalizado para la realizacion del CRUD
   const [users, getUsers, createUser, deleteUsers, updateUsers, loading] = useFetch(baseUrl);
